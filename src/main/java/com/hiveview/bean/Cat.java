9048423a8886274e31c172caebe5bbe8f86eee61
donghongliang <donghongliang@btte.net>
2018-04-20 20:58:51 +0800

    测试bean的生命周期

--- src/main/java/com/hiveview/bean/Cat.java
+++ src/main/java/com/hiveview/bean/Cat.java
@@ -11,13 +11,11 @@ public class Cat implements InitializingBean,DisposableBean {
 		System.out.println("cat constructor...");
 	}
 
-	@Override
 	public void destroy() throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("cat...destroy...");
 	}
 
-	@Override
 	public void afterPropertiesSet() throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("cat...afterPropertiesSet...");
